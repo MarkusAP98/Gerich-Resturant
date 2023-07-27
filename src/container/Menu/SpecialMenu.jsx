febf54d5f20aa4e6a572f52f5f9b8ad7e7f9da1c
Markus Phantha <markus.a.phantha@gmail.com>
2023-07-27 19:09:54 +0300

    started making the menu list

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 
+import { SubHeading, MenuItem } from '../../components';
+import { images, data} from '../../constants'
 import './SpecialMenu.css';
 
 const SpecialMenu = () => (
-  <div>
-    SpecialMenu
+  <div className='app__specialMenu flex__center__padding' id='menu'>
+    <div className=''>
+      <SubHeading title='Menu that fits your needs'/>
+      <h1 className='headtext__cormorant'>Today Special</h1>
+    </div>
+
+    <div className='app__specialMenu-menu'>  
+  <div className='app__specialMenu-menuwine flex__center'>
+<p className='app__specialMenu_menu_heading'>Wine & Beer</p>
+<div className='app__special_menu_item'>
+
+</div>
+  </div>
+    </div>
   </div>
 );
 
