cafc5505537b35af6949352c3fcf026258d52947
Markus Phantha <markus.a.phantha@gmail.com>
2023-09-21 16:33:08 +0300

    imported icons

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Footer.css';
 import { FooterOverlay, Newsletter } from '../../components'
+import { FiFacebook, FiTwitter, FiInstagram } from 'react-icons/Fi'
 
 const Footer = () => (
   <div>
