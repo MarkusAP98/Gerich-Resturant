b30591aa6ae6fe1c6183a3773d340e4ebbaa17a5
Markus Phantha <markus.a.phantha@gmail.com>
2023-09-21 16:48:29 +0300

    div footer

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './Footer.css';
 import { FooterOverlay, Newsletter } from '../../components'
-import { FiFacebook, FiTwitter, FiInstagram } from 'react-icons/Fi'
+import { FiFacebook, FiTwitter, FiInstagram } from 'react-icons/fi'
 
 const Footer = () => (
-  <div>
-    Footer
+  <div className='app__footer sectopn__padding '>
+    <FooterOverlay />
+    <Newsletter />
   </div>
 );
 
